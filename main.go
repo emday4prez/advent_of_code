6d08881fd9a546f03a763fd48315d004f38591a3
Emerson <emerson.day@gmail.com>
2024-12-02 21:45:53 -0800

    input reader

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/emday4prez/advent_of_code/one"
+
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Day 1:")
+	one.SolvePart1()
+	one.SolvePart2()
+
+	// fmt.Println("\nDay 2:")
+	// two.SolvePart1()
+	// two.SolvePart2()
+}
---
+++ one/one.go
@@ -0,0 +1,21 @@
+package one
+
+import "github.com/emday4prez/advent_of_code/utils"
+
+func SolvePart1() int {
+	lines, err := utils.ReadInput("one/input.txt")
+	if err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+
+	}
+
+	return 0
+}
+
+func SolvePart2() int {
+
+	return 1
+}
