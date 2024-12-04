71200d0fd6756ead2187a0fd605e881eb43d070d
Emerson <emerson.day@gmail.com>
2024-12-03 21:26:02 -0800

    day one part one

--- one/one.go
+++ one/one.go
@@ -1,18 +1,54 @@
 package one
 
-import "github.com/emday4prez/advent_of_code/utils"
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/emday4prez/advent_of_code/utils"
+)
 
 func SolvePart1() int {
 	lines, err := utils.ReadInput("one/input.txt")
 	if err != nil {
-		return err
+		return 1
 	}
+
+	left := []string{}
+	right := []string{}
 
 	for _, line := range lines {
+		inputs := strings.Split(line, "   ")
+		left = append(left, inputs[0])
+		right = append(right, inputs[1])
 
 	}
+	slices.Sort(left)
+	slices.Sort(right)
 
-	return 0
+	total := 0
+
+	for i := range 1000 {
+		a, err := strconv.Atoi(left[i])
+		if err != nil {
+			// ... handle error
+			panic(err)
+		}
+		b, err := strconv.Atoi(right[i])
+		if err != nil {
+			// ... handle error
+			panic(err)
+		}
+
+		if a > b {
+			total += a - b
+		} else {
+			total += b - a
+		}
+	}
+	fmt.Print(total)
+	return int(total)
 }
 
 func SolvePart2() int {
